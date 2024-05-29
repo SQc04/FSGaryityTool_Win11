701efa7ff6797aaa761dbe878053fec6e821527f
SQc04 <2327412600@qq.com>
2024-05-29 21:17:58 +0800

    Add credits and dependencies. Optimized the algorithm for sending messages.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -60,7 +60,7 @@ namespace FSGaryityTool_Win11
     public sealed partial class MainWindow : Window
     {
 
-        public static string FSSoftVersion = "0.2.24";
+        public static string FSSoftVersion = "0.2.26";
         public static int FsPage = 0;
         public static TomlTable settingstomlSp;
 
