bfe3f4fd89ccb7ce1843e035a8d5f9a216e3a116
Poker <poker_sang@outlook.com>
2024-09-29 21:22:14 +0800

    添加子模块

--- FSGaryityTool_Win11/MainWindow.xaml.cs
+++ FSGaryityTool_Win11/MainWindow.xaml.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
 using Microsoft.UI.Xaml.Data;
@@ -41,7 +41,6 @@ using System.Diagnostics;
 using System.IO;
 
 using Tommy;
-using Newtonsoft.Json;
 using System.Reflection.PortableExecutable;
 using Windows.ApplicationModel.Activation;
 using System.Xml.Linq;
